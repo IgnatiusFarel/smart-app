44fc0a54466dbd54483394e5b9e5034871829e3e
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-10 19:51:45 +0700

    [ADD] - OverView Page

---
+++ src/app/(dashboard)/persediaan/overview/page.tsx
@@ -0,0 +1,9 @@
+import OverViewPage from "./_components/overview";
+
+export const metadata = { 
+    title: 'Persediaan - Dashboard'
+};
+
+export default async function Page() {
+    return <OverViewPage />; 
+}
