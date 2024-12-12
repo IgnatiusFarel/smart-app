79d780d4906d8252605455352c4b6aa1cf3119ed
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-12 13:44:29 +0700

    [UPDATE] - Penerimaan Page

--- src/app/(dashboard)/persediaan/penerimaan/_components/penerimaan-view.tsx
+++ src/app/(dashboard)/persediaan/penerimaan/_components/penerimaan-view.tsx
@@ -9,6 +9,7 @@ import { dummyPenerimaan, Penerimaan } from "@/constants/data";
 import { TambahTransaksiModal } from "./_penerimaan-tables/modal-add";
 import PenerimaanTable from "./_penerimaan-tables/index";
 import PageContainer from "@/components/layout/page-container";
+import { CalendarDateRangePicker } from "@/components/date-range-picker";
 
 export default function PenerimaanViewPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -24,12 +25,16 @@ export default function PenerimaanViewPage() {
       <div className="flex items-start justify-between">
         <Heading title="Penerimaan" description="deskripsi penerimaan" />
 
-        <Button
+        <div className="hidden items-center space-x-2 md:flex">
+            <CalendarDateRangePicker />
+            <Button
           onClick={handleOpenModal}
           className={cn(buttonVariants({ variant: "default" }))}
         >
           <Plus className="mr-2 h-4 w-4" /> Tambah Transaksi
         </Button>
+          </div>
+     
       </div>
       <Separator />
 
