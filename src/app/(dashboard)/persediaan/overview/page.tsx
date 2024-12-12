95b4898a42c4f18ec3765ea681aefd43642c1560
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-12 13:44:03 +0700

    [DELETE] - Dashboard Page

--- src/app/(dashboard)/persediaan/overview/page.tsx
+++
@@ -1,9 +0,0 @@
-import OverViewPage from "./_components/overview";
-
-export const metadata = { 
-    title: 'Persediaan - Dashboard'
-};
-
-export default async function Page() {
-    return <OverViewPage />; 
-}
