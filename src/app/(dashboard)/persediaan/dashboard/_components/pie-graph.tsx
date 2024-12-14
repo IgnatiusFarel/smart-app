16a00a07aca6506ef318657fa7c8918bb43eae98
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-14 14:59:31 +0700

    [UPDATE] - Pie Graph

--- src/app/(dashboard)/persediaan/dashboard/_components/pie-graph.tsx
+++ src/app/(dashboard)/persediaan/dashboard/_components/pie-graph.tsx
@@ -1,72 +1,65 @@
-'use client';
-
-import * as React from 'react';
-import { TrendingUp } from 'lucide-react';
-import { Label, Pie, PieChart } from 'recharts';
-
+"use client";
+import * as React from "react";
+import { PieChart, Pie, Label, Tooltip } from "recharts";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
-  CardTitle
-} from '@/components/ui/card';
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
 import {
-  ChartConfig,
   ChartContainer,
   ChartTooltip,
-  ChartTooltipContent
-} from '@/components/ui/chart';
-const chartData = [
-  { browser: 'chrome', visitors: 275, fill: 'var(--color-chrome)' },
-  { browser: 'safari', visitors: 200, fill: 'var(--color-safari)' },
-  { browser: 'firefox', visitors: 287, fill: 'var(--color-firefox)' },
-  { browser: 'edge', visitors: 173, fill: 'var(--color-edge)' },
-  { browser: 'other', visitors: 190, fill: 'var(--color-other)' }
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData1 = [
+  { name: "A", value: 350, fill: "#730fc3" },
+  { name: "B", value: 350, fill: "#730fc3" },
+  { name: "C", value: 350, fill: "#730fc3" },
 ];
 
-const chartConfig = {
-  visitors: {
-    label: 'Visitors'
-  },
-  chrome: {
-    label: 'Chrome',
-    color: 'hsl(var(--chart-1))'
-  },
-  safari: {
-    label: 'Safari',
-    color: 'hsl(var(--chart-2))'
-  },
-  firefox: {
-    label: 'Firefox',
-    color: 'hsl(var(--chart-3))'
-  },
-  edge: {
-    label: 'Edge',
-    color: 'hsl(var(--chart-4))'
-  },
-  other: {
-    label: 'Other',
-    color: 'hsl(var(--chart-5))'
-  }
-} satisfies ChartConfig;
+const chartData2 = [
+  { name: "X", value: 500, fill: "#e8833a" },
+  { name: "Y", value: 400, fill: "#e8833a" },
+  { name: "Z", value: 300, fill: "#e8833a" },
+];
+
+const chartData3 = [
+  { name: "P", value: 700, fill: "#d3455b" },
+  { name: "Q", value: 500, fill: "#d3455b" },
+  { name: "R", value: 300, fill: "#d3455b" },
+];
+
+const chartConfig1 = {
+  A: { label: "Category A", color: "#730fc3" },
+  B: { label: "Category B", color: "#730fc3" },
+  C: { label: "Category C", color: "#730fc3" },
+};
+
+const chartConfig2 = {
+  X: { label: "Category X", color: "#e8833a" },
+  Y: { label: "Category Y", color: "#e8833a" },
+  Z: { label: "Category Z", color: "#e8833a" },
+};
+
+const chartConfig3 = {
+  P: { label: "Category P", color: "#d3455b" },
+  Q: { label: "Category Q", color: "#d3455b" },
+  R: { label: "Category R", color: "#d3455b" },
+};
 
 export function PieGraph() {
-  const totalVisitors = React.useMemo(() => {
-    return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
-  }, []);
-
   return (
-    <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+    <Card className="flex flex-col items-center">
+      <CardHeader className="pb-4">
+        <CardTitle>Total Nilai Persediaan (Rp.)</CardTitle>
       </CardHeader>
-      <CardContent className="flex-1 pb-0">
+      <CardContent className="flex justify-around w-full gap-4">
         <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[360px]"
+          config={chartConfig1}
+          className="mx-auto aspect-square max-h-[180px] min-h-[180px] min-w-[180px]"
         >
           <PieChart>
             <ChartTooltip
@@ -74,15 +67,16 @@ export function PieGraph() {
               content={<ChartTooltipContent hideLabel />}
             />
             <Pie
-              data={chartData}
-              dataKey="visitors"
-              nameKey="browser"
-              innerRadius={60}
-              strokeWidth={5}
+              data={chartData1}
+              dataKey="value"
+              nameKey="name"
+              innerRadius={40}
+              outerRadius={70}
+              strokeWidth={3}
             >
               <Label
                 content={({ viewBox }) => {
-                  if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
                     return (
                       <text
                         x={viewBox.cx}
@@ -93,16 +87,95 @@ export function PieGraph() {
                         <tspan
                           x={viewBox.cx}
                           y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold"
+                          className="fill-foreground text-xl font-bold"
                         >
-                          {totalVisitors.toLocaleString()}
+                          1000
                         </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+
+        <ChartContainer
+          config={chartConfig2}
+          className="mx-auto aspect-square max-h-[180px] min-h-[180px] min-w-[180px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData2}
+              dataKey="value"
+              nameKey="name"
+              innerRadius={40}
+              outerRadius={70}
+              strokeWidth={3}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
                         <tspan
                           x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 24}
-                          className="fill-muted-foreground"
+                          y={viewBox.cy}
+                          className="fill-foreground text-xl font-bold"
                         >
-                          Visitors
+                          2000
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+
+        <ChartContainer
+          config={chartConfig3}
+          className="mx-auto aspect-square max-h-[180px] min-h-[180px] min-w-[180px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData3}
+              dataKey="value"
+              nameKey="name"
+              innerRadius={40}
+              outerRadius={70}
+              strokeWidth={3}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-xl font-bold"
+                        >
+                        3000
                         </tspan>
                       </text>
                     );
@@ -113,12 +186,11 @@ export function PieGraph() {
           </PieChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+      <CardFooter className="text-sm font-medium w-full">
+        <div className="flex justify-between w-full">
+          <div className="pl-20 text-[#730fc3]">Kelompok Material</div>
+          <div className="text-[#e8833a]">Lokasi Gudang</div>
+          <div className="pr-20 text-[#d3455b]">Kategori Material</div>
         </div>
       </CardFooter>
     </Card>
