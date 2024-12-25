19c904e82b4f864f5333ed7ab3cdeef72fbebb33
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-25 20:35:05 +0700

    [UPDATE] - HBB Inventaris - Penghapusan

---
+++ src/app/(dashboard)/hbb-inventaris/penghapusan/page.tsx
@@ -0,0 +1,9 @@
+import PenghapusanViewPage from './_components/penghapusan-view';
+
+export const metadata = {
+  title: 'HBB Inventaris - Penghapusan'
+};
+
+export default function page() {
+  return <PenghapusanViewPage />;
+}
