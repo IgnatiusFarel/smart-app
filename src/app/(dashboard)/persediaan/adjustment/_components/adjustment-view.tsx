d004c17c00620e4dd6c94686522b50772c27f6ff
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-29 23:58:53 +0700

    [UPDATE] - Persediaan - Adjustment

--- src/app/(dashboard)/persediaan/adjustment/_components/adjustment-view.tsx
+++ src/app/(dashboard)/persediaan/adjustment/_components/adjustment-view.tsx
@@ -9,6 +9,7 @@ import { Plus } from "lucide-react";
 import AdjustmentTable from "./_adjustment-tables/index";
 import { dummyAdjustment, Adjustment } from "@/constants/data";
 import { TambahTransaksiModal } from "./_adjustment-tables/modal-add";
+import { CalendarDateRangePicker } from "@/components/date-range-picker";
 
 export default function AdjustmentViewPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -21,15 +22,18 @@ export default function AdjustmentViewPage() {
   return (
     <PageContainer scrollable>
       <div className="space-y-4">
-        <div className="flex items-start justify-between">
+        <div className="flex items-center justify-between">
           <Heading title="Adjustment" description="deskripsi Adjustment" />
 
+          <div className="flex flex-col md:flex-col lg:flex-row lg:items-center lg:space-x-2 space-y-2 lg:space-y-0">
+          <CalendarDateRangePicker />
           <Button
             onClick={handleOpenModal}
             className={cn(buttonVariants({ variant: "default" }))}
           >
             <Plus className="mr-2 h-4 w-4" /> Tambah Transaksi
           </Button>
+        </div>
         </div>
         <Separator />
 
