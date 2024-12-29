987ded7d5f14bb0a956f396cd46e8a357492606e
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-29 23:59:15 +0700

    [UPDATE] - Persediaan - Bank Data

--- src/app/(dashboard)/persediaan/bank-data/_components/bankdata-view.tsx
+++ src/app/(dashboard)/persediaan/bank-data/_components/bankdata-view.tsx
@@ -50,7 +50,7 @@ export default function BankDataViewPage() {
               <Plus className="mr-2 h-4 w-4" /> Tambah Data
             </Button>
           </div>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid md:grid-cols-3 grid-cols-2  gap-4">
             <BankDataTypeButton
               onClick={() => handleViewChange("default")}
               isActive={activeButton === "default"}
