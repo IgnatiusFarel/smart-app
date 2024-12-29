34adbb1b494ea9f9018d8f2ce68a66dd5d1ee210
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-30 00:00:25 +0700

    [UPDATE]  - Persediaan - Penerimaan

--- src/app/(dashboard)/persediaan/penerimaan/_components/penerimaan-view.tsx
+++ src/app/(dashboard)/persediaan/penerimaan/_components/penerimaan-view.tsx
@@ -5,7 +5,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
-import { dummyPenerimaan, Penerimaan } from "@/constants/data";
+import { dummyPersediaanPenerimaan, PersediaanPenerimaan } from "@/constants/data";
 import { TambahTransaksiModal } from "./_penerimaan-tables/modal-add";
 import PenerimaanTable from "./_penerimaan-tables/index";
 import PageContainer from "@/components/layout/page-container";
@@ -13,8 +13,8 @@ import { CalendarDateRangePicker } from "@/components/date-range-picker";
 
 export default function PenerimaanViewPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const totalPenerimaan = dummyPenerimaan.length;
-  const penerimaan: Penerimaan[] = dummyPenerimaan;
+  const totalPenerimaan = dummyPersediaanPenerimaan.length;
+  const penerimaan: PersediaanPenerimaan[] = dummyPersediaanPenerimaan;
 
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
@@ -22,10 +22,10 @@ export default function PenerimaanViewPage() {
   return (
     <PageContainer scrollable>
       <div className="space-y-4">
-        <div className="flex items-start justify-between">
+        <div className="flex items-center justify-between">
           <Heading title="Penerimaan" description="deskripsi penerimaan" />
 
-          <div className="hidden items-center space-x-2 md:flex">
+          <div className="flex flex-col md:flex-col lg:flex-row lg:items-center lg:space-x-2 space-y-2 lg:space-y-0">
             <CalendarDateRangePicker />
             <Button
               onClick={handleOpenModal}
