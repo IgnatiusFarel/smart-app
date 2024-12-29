ed2e29f4ec6fed764a1816df51b04410dcdc810c
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-30 00:05:10 +0700

    [ADD] - QR Code - Table Filters

---
+++ src/app/(dashboard)/persediaan/qr-code/_components/qrcode-tables/table-filters.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useCallback, useMemo } from "react";
+import { useQueryState } from "nuqs";
+import { searchParams } from "@/lib/searchparams";
+
+export function useQRCodeTableFilters() {
+  const [searchQuery, setSearchQuery] = useQueryState(
+    'search',
+    searchParams.q.withOptions({ shallow: false }).withDefault('')
+  );
+
+  const [page, setPage] = useQueryState(
+    'page',
+    searchParams.page.withDefault(1)
+  );
+
+  const resetFilters = useCallback(() => {
+    setSearchQuery(null);
+
+    setPage(1);
+  }, [setSearchQuery, setPage]);
+
+  const isAnyFilterActive = useMemo(() => {
+    return !!searchQuery;
+  }, [searchQuery]);
+
+  return {
+    searchQuery,
+    setSearchQuery,
+    page,
+    setPage,
+    resetFilters,
+    isAnyFilterActive
+  };
+}
