0fc7a51f1584a6ea76618dfaedbb4fd8bfa76697
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-31 21:39:48 +0700

    [UPDATE] - HBB Inventaris - Penerimaan

--- src/app/(dashboard)/hbb-inventaris/penerimaan/_components/_penerimaan-tables/columns.tsx
+++ src/app/(dashboard)/hbb-inventaris/penerimaan/_components/_penerimaan-tables/columns.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
 import { CellAction } from "./cell-action";
-import { Penerimaan } from "@/constants/data";
+import { HBBInventarisPenerimaan } from "@/constants/data";
+import { Badge } from "@/components/ui/badge";
 
-export const columns: ColumnDef<Penerimaan>[] = [
+export const columns: ColumnDef<HBBInventarisPenerimaan>[] = [
   {
     accessorKey: 'no',
     header: 'No',
@@ -22,7 +23,20 @@ export const columns: ColumnDef<Penerimaan>[] = [
   {
     accessorKey: "approvalStatus",
     header: "Approval Status",
-    cell: ({ row }) => row.original.approvalStatus,
+    cell: ({ row }) => {
+      const status = row.original.approvalStatus;
+
+      let badgeColor = ""; 
+      if (status === "Rejected") {
+        badgeColor = "bg-[#d3455b]";
+      } else if (status === "Approved") {
+        badgeColor = "bg-[#1aae9f]";
+      } else if (status === "Waiting") {
+        badgeColor = "bg-[#788896]";
+      }
+
+      return <Badge className={badgeColor}>{status}</Badge>;
+    },
   },
   {
     accessorKey: "kodeBarang",
