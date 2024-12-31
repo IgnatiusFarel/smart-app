deeb104316ae452382c0ff2d4e2216760f2fc23e
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-31 21:38:54 +0700

    [UPDATE] - Activity Log

--- src/app/(dashboard)/activity-log/activity-log/_components/activitylog-view.tsx
+++ src/app/(dashboard)/activity-log/activity-log/_components/activitylog-view.tsx
@@ -1,43 +1,28 @@
 "use client";
 import PageContainer from "@/components/layout/page-container";
-// import { Button, buttonVariants } from "@/components/ui/button";
-// import { useState } from "react";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-// import { cn } from "@/lib/utils";
-// import { Plus } from "lucide-react";
 import { dummyActivityLog, ActivityLog } from "@/constants/data";
 import { CalendarDateRangePicker } from "@/components/date-range-picker";
 import ActivityLogTable from "./_activitylog-tables/index";
 
 export default function ActivityLogViewPage() {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
   const totalActivityLog = dummyActivityLog.length;
   const activityLog: ActivityLog[] = dummyActivityLog;
-
-//   const handleOpenModal = () => setIsModalOpen(true);
-//   const handleCloseModal = () => setIsModalOpen(false);
 
   return (
     <PageContainer scrollable>
       <div className="space-y-4">
         <div className="flex items-start justify-between">
-          <Heading title="User Management" description="deskripsi user management" />
-
-          {/* <div className="hidden items-center space-x-2 md:flex"> */}
+          <Heading
+            title="Activity Log"
+            description="deskripsi activity log"
+          />
           <CalendarDateRangePicker />
-          {/* <Button
-            onClick={handleOpenModal}
-            className={cn(buttonVariants({ variant: "default" }))}
-          >
-            <Plus className="mr-2 h-4 w-4" /> Tambah User
-        //   </Button> */}
-        {/* // </div> */}
         </div>
         <Separator />
 
         <ActivityLogTable data={activityLog} totalData={totalActivityLog} />
-     
       </div>
     </PageContainer>
   );
