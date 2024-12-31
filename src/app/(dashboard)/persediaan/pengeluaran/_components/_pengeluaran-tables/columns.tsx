5cfa9e41f4c2de8c4b89154fde2076f76c900544
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2024-12-31 21:46:13 +0700

    [UPDATE] - Persediaan - Pengeluaran

--- src/app/(dashboard)/persediaan/pengeluaran/_components/_pengeluaran-tables/columns.tsx
+++ src/app/(dashboard)/persediaan/pengeluaran/_components/_pengeluaran-tables/columns.tsx
@@ -2,9 +2,10 @@
 import { Checkbox } from '@/components/ui/checkbox';
 import { ColumnDef } from '@tanstack/react-table';
 import { CellAction } from './cell-action';
-import { Penerimaan } from '@/constants/data';
+import { Pengeluaran } from '@/constants/data';
+import { Badge } from '@/components/ui/badge';
 
-export const columns: ColumnDef<Penerimaan>[] = [
+export const columns: ColumnDef<Pengeluaran>[] = [
   {
     id: 'select',
     header: ({ table }) => (
@@ -42,7 +43,20 @@ export const columns: ColumnDef<Penerimaan>[] = [
   {
     accessorKey: 'approvalStatus',
     header: 'Approval Status',
-    cell: ({ row }) => row.original.approvalStatus,
+    cell: ({ row }) => {
+      const status = row.original.approvalStatus;
+
+      let badgeColor = ""; 
+      if (status === "Rejected") {
+        badgeColor = "bg-[#d3455b]";
+      } else if (status === "Approved") {
+        badgeColor = "bg-[#1aae9f]";
+      } else if (status === "Waiting") {
+        badgeColor = "bg-[#788896]";
+      }
+
+      return <Badge className={badgeColor}>{status}</Badge>;
+    },
   },
   {
     accessorKey: 'kodeBarang',
